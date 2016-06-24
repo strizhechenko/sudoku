c404f1e75ef3eb468d065aeb39ea2cd904fede14
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-06-24 16:23:48 +0600

    Update validator.py

--- validator.py
+++ validator.py
@@ -1,7 +1,41 @@
+from operator import add
 from numpy import matrix
 
+wrong_sudoku = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+                [6, 7, 2, 1, 9, 0, 3, 4, 9],
+                [1, 0, 0, 3, 4, 2, 5, 6, 0],
+                [8, 5, 9, 7, 6, 1, 0, 2, 0],
+                [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                [9, 0, 1, 5, 3, 7, 2, 1, 4],
+                [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                [3, 0, 0, 4, 8, 1, 1, 7, 9]]
+
+good_sudoku = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+               [6, 7, 2, 1, 9, 5, 3, 4, 8],
+               [1, 9, 8, 3, 4, 2, 5, 6, 7],
+               [8, 5, 9, 7, 6, 1, 4, 2, 3],
+               [4, 2, 6, 8, 5, 3, 7, 9, 1],
+               [7, 1, 3, 9, 2, 4, 8, 5, 6],
+               [9, 6, 1, 5, 3, 7, 2, 8, 4],
+               [2, 8, 7, 4, 1, 9, 6, 3, 5],
+               [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+
+
+blocks = ((0, 3), (3, 6), (6, 9))
 valid_digits = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-blocks = [[1] * 3, [0] * 3 + [1] * 3, [0] * 6 + [1] * 3]
+
+
+def flatx(m):
+    return matrix(m).flatten().tolist()[0]
+
+
+def flat(m):
+    return reduce(add, m)
+
+
+def validValues(board):
+    return set(flat(board)) == valid_digits
 
 
 def validRows(board):
@@ -12,13 +46,12 @@ def validColumns(board):
     return validRows(zip(*board))
 
 
+def flatblock(x, y, m):
+    return flat([sub[x[0]:x[1]] for sub in m[y[0]:y[1]]])
+
+
 def validBlocks(board):
-    flatblock = lambda x, y: matrix(board).compress(x, 0).compress(y, 1).flatten().tolist()[0]
-    return all(all(set(flatblock(x, y)) == valid_digits for y in blocks) for x in blocks)
-
-
-def validValues(board):
-    return set(matrix(board).flatten().tolist()[0]) == valid_digits
+    return all(all(validValues(flatblock(x, y, board)) for y in blocks) for x in blocks)
 
 
 def validSolution(board):
